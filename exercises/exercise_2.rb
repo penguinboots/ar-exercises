9bf1527229359c5ef598d286d6603b4ed29d3386
penguinboots <sabrina.ynw@gmail.com>
2023-05-15 12:33:15 -0700

    update store1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -7,6 +7,6 @@ puts "----------"
 # Your code goes here ...
 
 @store1 = Store.find_by(id: 1)
-@store1.destroy
+@store2 = Store.find_by(id: 2)
 
-puts Store.count
+@store1.update(annual_revenue: 340000)
