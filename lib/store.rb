9002225e193e499c914e6a9fa600f57772babe40
penguinboots <sabrina.ynw@gmail.com>
2023-05-15 13:09:27 -0700

    validation for both models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
-    has_many :employees
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, minimum: 0 }
+  validate :either_apparel
+
+  def either_apparel
+    if !womens_apparel && !mens_apparel
+      errors.add(:mens_apparel, "Either men's or women's apparel must be true")
+    end
+  end
+
 end
